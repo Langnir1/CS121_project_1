fb1e3fe5a20288512a936c349bc1b75324a43175
Langnir1 <joshua.halamka@bsu.edu>
2025-01-10 15:47:00 +0000

    The previous code crashed. I'm somewhat dumb

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 print("hello world")
 
-name = userInput("What is your name?: ")
-print("Good day " + name +". I hope your day, nay, your week is of the utmost smooth sailing.")
+name = input("What is your name?: ")
+print(f"Good day {name}! I hope your day, nay, your week is of the utmost smooth sailing.")
